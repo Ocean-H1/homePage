23b54cf3f4a7006cac7ee4b9a5331f669342f640
OceanH <1404939185@qq.com>
2024-08-11 19:02:08 +0800

    feat: 修改google_api访问权限

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+//  敏感信息
+
+const GOOGLE_CALENDAR_APIKEY = 'AIzaSyDygjEzhmnE0ZmcxbS28OQciXdOJgz7ycE'
+
+const GOOGLE_CALENDAR_ID =
+  '5cd5a5d38190fee8a7bee9e457dae2812df3d940a0f417bfbc823847e7ef39eb@group.calendar.google.com'
